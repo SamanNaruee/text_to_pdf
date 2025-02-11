dd5a92b46af8042e1220949f3884cd1e20f12e80
Saman-naruee <samannaruee@gmail.com>
2025-02-11 14:50:13 +0330

    udpate... .

--- convert_text_into_pdf.py
+++ convert_text_into_pdf.py
@@ -1,5 +1,15 @@
 from fpdf import FPDF
 
+
+def file_name_converter(file_path):
+    file_name = file_path.split('/')[-1]
+    
+    if file_name.endswith('.srt'):
+        return f"{file_path}/{file_name.replace('.srt', '.txt')}"
+    else:
+        raise ValueError("File must be a .srt file")
+
+
 def create_pdf(text, filename):
     pdf = FPDF()
     pdf.add_page()
@@ -15,9 +25,11 @@ def read_text_from_file(file_path):
     
 
 
-text_dir = lambda: input("Enter the path to your text file: ")()
+text_dir = str(input("Enter the text file path: "))
 text = read_text_from_file(text_dir)
 
 filename = "Converted.pdf"
 create_pdf(text, filename)
 print(f"PDF created successfully: {filename}")
+
+print(file_name_converter("C:/Users/admin/Downloads/The.Sixth.Sense-en.srt"))
